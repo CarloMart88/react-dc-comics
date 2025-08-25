0d4075ca802a5422eae1311b4290ba8b8f061b84
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-25 17:28:43 +0200

    feat: add DC logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,23 +11,28 @@ const Footer = () => {
           </li>
           <li id="flex">
             <img src="../img/buy-comics-merchandise.png" id="size" />
-            <p className="position">DIGITAL COMICS</p>
-          </li>
-          <li id="flex">
-            <img src="../img/buy-comics-shop-locator.png" id="size" />
-            <p className="position">DIGITAL COMICS</p>
+            <p className="position">DC MERCHANDISE</p>
           </li>
           <li id="flex">
             <img src="../img/buy-comics-subscriptions.png" id="size" />
-            <p className="position">DIGITAL COMICS</p>
+            <p className="position">SUBSCRIPTION</p>
+          </li>
+          <li id="flex">
+            <img src="../img/buy-comics-shop-locator.png" id="size" />
+            <p className="position">COMIC SHOP LOCATOR</p>
           </li>
           <li id="flex">
             <img src="../img/buy-dc-power-visa.svg" id="size" />
-            <p className="position">DIGITAL COMICS</p>
+            <p className="position">DC POWER VISA</p>
           </li>
         </ul>
       </div>
-      <div className="foot-bg"></div>
+      <div className="foot-bg">
+        <div className="fifthy"></div>
+        <div className="fifthy">
+          <img src="../img/dc-logo-bg.png" className="dc_button" />
+        </div>
+      </div>
       
     </footer>
   )
