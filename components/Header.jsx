c89682ef7cb9d53e29fe589cd20710981c62a1f8
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-25 15:51:14 +0200

    feat: work on the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,25 @@
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div className="container-fluid">
+        <div id="logo"></div>
+        <div id="lista_header">
+          <ul id="Hlist">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </header>
   )
 }
 
