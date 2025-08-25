aff675360f9041b1f1a442e92efb9d751d13583c
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-25 17:14:46 +0200

    feat: first part of the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,34 @@
 
 const Footer = () => {
   return (
-    <div>
+    <footer>
+      <div className="bg-blue">
+        <ul className="lista-footer">
+          <li id="flex">
+            <img src="../img/buy-comics-digital-comics.png" id="size" />
+            <p className="position">DIGITAL COMICS</p>
+          </li>
+          <li id="flex">
+            <img src="../img/buy-comics-merchandise.png" id="size" />
+            <p className="position">DIGITAL COMICS</p>
+          </li>
+          <li id="flex">
+            <img src="../img/buy-comics-shop-locator.png" id="size" />
+            <p className="position">DIGITAL COMICS</p>
+          </li>
+          <li id="flex">
+            <img src="../img/buy-comics-subscriptions.png" id="size" />
+            <p className="position">DIGITAL COMICS</p>
+          </li>
+          <li id="flex">
+            <img src="../img/buy-dc-power-visa.svg" id="size" />
+            <p className="position">DIGITAL COMICS</p>
+          </li>
+        </ul>
+      </div>
+      <div className="foot-bg"></div>
       
-    </div>
+    </footer>
   )
 }
 
